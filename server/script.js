c75ef38af7b675afe1d241b6ae9c6cacad7d6109
ferdinant722 <oferdinant321@gmail.com>
2024-04-16 11:39:29 +0300

    ammend

--- server/script.js
+++ server/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://project-openai-codex-1.onrender.com', {
+    const response = await fetch('https://project_openai_codex-1.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
